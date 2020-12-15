949d1c263b8b061f46697d3558a39dfc3bf4126f
Oliver Sandoval <sandovalot@hotmail.com>
2020-12-15 01:17:21 -0500

    agregada funcion de detalle de usuario, corrección en codigo para optimizarlo y refactorizarlo, hacer entendible el codigo

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -7,14 +8,49 @@ from apps.users.api.serializers import UserSerializer
 @api_view(['GET','POST'])
 def user_api_view(request):
 
+    # list
     if request.method == 'GET':
+        # queryset
         users = User.objects.all()
         users_serializer = UserSerializer(users,many = True)
-        return Response(users_serializer.data)
+        
+        return Response(users_serializer.data,status = status.HTTP_200_OK)
     
+    # create
     elif request.method == 'POST':
         user_serializer = UserSerializer(data = request.data)
+        
+        # validation
         if user_serializer.is_valid():
-            user_serializer.save()
-            return Response(user_serializer.data)
-        return Response(user_serializer.errors)
+            user_serializer.save()            
+            return Response({'message':'Usuario creado correctamente!'},status = status.HTTP_201_CREATED)
+        
+        return Response(user_serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def user_detail_api_view(request,pk=None):
+    # queryset
+    user = User.objects.filter(id = pk).first()
+
+    # validation
+    if user:
+
+        # retrieve
+        if request.method == 'GET':            
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data,status = status.HTTP_200_OK)
+        
+        # update
+        elif request.method == 'PUT':
+            user_serializer = UserSerializer(user,data = request.data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data,status = status.HTTP_200_OK)
+            return Response(user_serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+        
+        # delete
+        elif request.method == 'DELETE':
+            user.delete()
+            return Response({'message':'Usuario Eliminado correctamente!'},status = status.HTTP_200_OK)
+    
+    return Response({'message':'No se ha encontrado un usuario con estos datos'},status = status.HTTP_400_BAD_REQUEST)
--- ecommerce_rest/settings/local.py
+++ ecommerce_rest/settings/local.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -11,7 +12,7 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
