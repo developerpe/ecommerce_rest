489c4fb61ccfe8030037fb73ae842dc95c8a58af
Oliver Sandoval <sandovalot@hotmail.com>
2021-03-09 02:21:32 -0500

    logout agregado

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.contrib.sessions.models import Session
 
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -25,19 +26,25 @@ class Login(ObtainAuthToken):
                         'message': 'Inicio de Sesión Exitoso.'
                     }, status = status.HTTP_201_CREATED)
                 else:
+                    """
                     all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
                     if all_sessions.exists():
                         for session in all_sessions:
                             session_data = session.get_decoded()
                             if user.id == int(session_data.get('_auth_user_id')):
                                 session.delete()
-                    token.delete()
+                    
                     token = Token.objects.create(user = user)
                     return Response({
                         'token': token.key,
                         'user': user_serializer.data,
                         'message': 'Inicio de Sesión Exitoso.'
                     }, status = status.HTTP_201_CREATED)
+                    """
+                    token.delete()
+                    return Response({
+                        'error': 'Ya se ha iniciado sesión con este usuario.'
+                    }, status = status.HTTP_409_CONFLICT)
             else:
                 return Response({'error':'Este usuario no puede iniciar sesión.'}, 
                                     status = status.HTTP_401_UNAUTHORIZED)
@@ -45,3 +52,35 @@ class Login(ObtainAuthToken):
             return Response({'error': 'Nombre de usuario o contraseña incorrectos.'},
                                     status = status.HTTP_400_BAD_REQUEST)
         return Response({'mensaje':'Hola desde response'}, status = status.HTTP_200_OK)
+
+class Logout(APIView):
+
+    def get(self,request,*args,**kwargs):
+        try:
+            token = request.GET.get('token')
+            token = Token.objects.filter(key = token).first()
+
+            if token:
+                user = token.user
+                
+                all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
+                if all_sessions.exists():
+                    for session in all_sessions:
+                        session_data = session.get_decoded()
+                        if user.id == int(session_data.get('_auth_user_id')):
+                            session.delete()
+                
+                token.delete()
+                
+                session_message = 'Sesiones de usuario eliminadas.'  
+                token_message = 'Token eliminado.'
+                return Response({'token_message': token_message,'session_message':session_message},
+                                    status = status.HTTP_200_OK)
+            
+            return Response({'error':'No se ha encontrado un usuario con estas credenciales.'},
+                    status = status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'error': 'No se ha encontrado token en la petición.'}, 
+                                    status = status.HTTP_409_CONFLICT)
+        
+        
--- ecommerce_rest/urls.py
+++ ecommerce_rest/urls.py
@@ -6,7 +6,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from apps.users.views import Login
+from apps.users.views import Login,Logout
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -26,6 +26,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('logout/', Logout.as_view(), name = 'logout'),
     path('',Login.as_view(), name = 'login'),
     path('usuario/',include('apps.users.api.urls')),
     path('products/',include('apps.products.api.routers')),
