d7a9bcb3473f5d46b5900f6cba41e082a8c18c83
Oliver Sandoval <sandovalot@hotmail.com>
2021-07-14 17:55:50 -0500

    eliminacion de categoria de producto agregada

--- apps/products/api/views/general_views.py
+++ apps/products/api/views/general_views.py
@@ -11,23 +11,23 @@ class MeasureUnitViewSet(viewsets.GenericViewSet):
     serializer_class = MeasureUnitSerializer
 
     def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
+        return self.get_serializer().Meta.model.objects.filter(state=True)
 
-    def list(self,request):
+    def list(self, request):
         data = self.get_queryset()
-        data = self.get_serializer(data,many = True)
+        data = self.get_serializer(data, many=True)
         return Response(data.data)
     
-    def create(self,request):
+    def create(self, request):
         return Response({})
 
 class IndicatorViewSet(viewsets.GenericViewSet):
     serializer_class = IndicatorSerializer
 
     def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
+        return self.get_serializer().Meta.model.objects.filter(state=True)
 
-    def list(self,request):
+    def list(self, request):
         data = self.get_queryset()
         data = self.get_serializer(data,many = True)
         return Response(data.data)
@@ -36,16 +36,29 @@ class CategoryProductViewSet(viewsets.GenericViewSet):
     serializer_class = CategoryProductSerializer
 
     def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
+        return self.get_serializer().Meta.model.objects.filter(state=True)
 
-    def list(self,request):
+    def list(self, request):
         data = self.get_queryset()
-        data = self.get_serializer(data,many = True)
+        data = self.get_serializer(data, many=True)
         return Response(data.data)
 
-    def create(self,request):
-        serializer = self.serializer_class(data = request.data)
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message':'Categoría registrada correctamente!'},status = status.HTTP_201_CREATED)
-        return Response({'message':'','error':serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({'message':'Categoría registrada correctamente!'}, status=status.HTTP_201_CREATED)
+        return Response({'message':'', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+        serializer = self.serializer_class(instance=self.get_object(), data=request.data)
+        if serializer.is_valid():       
+            serializer.save()       
+            return Response({'message':'Categoría actualizada correctamente!'}, status=status.HTTP_200_OK)       
+        return Response({'message':'', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk):
+        instance = self.get_object()
+        instance.state = False
+        instance.save()
+        return Response({'message':'Categoría eliminada correctamente!'}, status=status.HTTP_200_OK)
